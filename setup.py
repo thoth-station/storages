cba0cb0e8cab0589019e51bb4bda27ac4ba725fd
Fridolin Pokorny <fridolin@redhat.com>
2018-02-21 13:57:41 +0100

    Initial project import

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='thoth-storages',
+    version='0.0.0',
+    description='Storage and database adapters available in project Thoth',
+    long_description='Storage and database adapters available in project Thoth',
+    author='Fridolin Pokorny',
+    author_email='fridolin@redhat.com',
+    license='GPLv2+',
+    packages=['thoth.storages'],
+    zip_safe=False,
+)
