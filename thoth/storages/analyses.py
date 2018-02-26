aa28c6705caa20f7039543890422392731b3926d
Fridolin Pokorny <fridolin@redhat.com>
2018-02-26 13:00:18 +0100

    Add docstrings for result store methods.

--- thoth/storages/analyses.py
+++ thoth/storages/analyses.py
@@ -13,6 +13,7 @@ class AnalysisResultsStore(object):
         self.host = host or os.environ['THOTH_ANALYSIS_RESULTS_STORE_HOST']
 
     def retrieve_by_document_id(self, document_id: str) -> dict:
+        """Retrieve document stored in analysis results store by its ID."""
         assert document_id.startswith('analysis-'), \
             "Please make sure you are calling right adapter to retrieve results."
         response = requests.get('{}/api/v1/result/{}'.format(self.host, document_id))
@@ -20,11 +21,13 @@ class AnalysisResultsStore(object):
         return response.json()
 
     def store_document(self, content: dict) -> str:
+        """Store the given document content, return an ID of the stored document."""
         response = requests.post('{}/api/v1/analysis-result'.format(self.host), json=content)
         response.raise_for_status()
         return response.json()['document_id']
 
     def get_result_listing(self):
+        """List all available analysis results stored."""
         response = requests.get('{}/api/v1/result?type=analysis'.format(self.host))
         response.raise_for_status()
         return response.json()['files']
--- thoth/storages/solvers.py
+++ thoth/storages/solvers.py
@@ -16,22 +16,26 @@ class SolverResultsStore(object):
         self.host = host or self.DEFAULT_HOST
 
     def retrieve_by_document_id(self, document_id: str) -> dict:
+        """Retrieve document stored in solver results store by its ID."""
         assert document_id.startswith('solver-'), "Please make sure you are calling right adapter to retrieve results."
         response = requests.get('{}/api/v1/result/{}'.format(self.host, document_id))
         response.raise_for_status()
         return response.json()
 
     def store_document(self, content: dict) -> str:
+        """Store the given document content, return an ID of the stored document."""
         response = requests.post('{}/api/v1/solver-result'.format(self.host), json=content)
         response.raise_for_status()
         return response.json()['document_id']
 
     def get_result_listing(self):
+        """List all available solver results stored."""
         response = requests.get('{}/api/v1/result?type=solver'.format(self.host))
         response.raise_for_status()
         return response.json()['files']
 
     def iterate_results(self):
+        """Iterate over available solver results, yielding one document at the time."""
         for document_id in self.get_result_listing():
             document = self.retrieve_by_document_id(document_id)
             yield document_id, document
