fd50ba240229200b1d92059553bc21a810d9671a
Fridolin Pokorny <fridolin@redhat.com>
2018-03-05 09:45:48 +0100

    Add base classes for vertex and edges to cover common logic

--- thoth/storages/graph/models.py
+++ thoth/storages/graph/models.py
@@ -4,14 +4,40 @@ from goblin import element
 from goblin import properties
 
 
-class PackageVersion(element.Vertex):
+class VertexBase(element.Vertex):
+    """A base class for edges that extends Goblin's vertex implementation."""
+
+    @classmethod
+    def create(cls, **vertex_properties):
+        """Create vertex based on its properties.
+
+        See :meth:`thoth.storages.graph.models.EdgeBase.create`.
+        """
+        return cls.from_dict(vertex_properties)
+
+
+class EdgeBase(element.Edge):
+    """A base class for edges that extends Goblin's edge implementation."""
+
+    @classmethod
+    def create(cls, **edge_properties):
+        """Create edge based on its properties.
+
+        >>> source_node = PackageVersion.create(ecosystem='pypi', name='selinon', version='1.0.0rc1')
+        >>> target_node = PackageVersion.create(ecosystem='pypi', name='pyyaml', version='1.0.0')
+        >>> edge = DependsOn.create(version_range='>=10', source=source_node, target=target_node)
+        """
+        return cls.from_dict(edge_properties)
+
+
+class PackageVersion(VertexBase):
     """Package-version node in graph representing any versioned package."""
     ecosystem = properties.Property(properties.String)
     name = properties.Property(properties.String)
     version = properties.Property(properties.String)
 
 
-class DependsOn(element.Edge):
+class DependsOn(EdgeBase):
     """Dependency between packages modeling."""
     version_range = properties.Property(properties.String, default='*')
     document_id = properties.Property(properties.String)
