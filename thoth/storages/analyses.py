0e42c3c1d0b05d5a212af6a6aa77b2c7dbcbadf8
Fridolin Pokorny <fridolin@redhat.com>
2018-03-13 14:57:01 +0100

    Reuse logic in analysis adapter from result base adapter

--- thoth/storages/analyses.py
+++ thoth/storages/analyses.py
@@ -1,41 +1,7 @@
 """Adapter for storing analysis results onto a persistence remote store."""
 
-import os
-
-import requests
-
-from .base import StorageBase
+from .result_base import ResultStorageBase
 
 
-class AnalysisResultsStore(StorageBase):
-    """Adapter for storing analysis results."""
-
-    def __init__(self, host: str=None):
-        super().__init__()
-        self.host = host or os.environ['THOTH_ANALYSIS_RESULTS_STORE_HOST']
-
-    def retrieve_by_document_id(self, document_id: str) -> dict:
-        """Retrieve document stored in analysis results store by its ID."""
-        assert document_id.startswith('analysis-'), \
-            "Please make sure you are calling right adapter to retrieve results."
-        response = requests.get('{}/api/v1/result/{}'.format(self.host, document_id))
-        response.raise_for_status()
-        return response.json()
-
-    def store_document(self, content: dict) -> str:
-        """Store the given document content, return an ID of the stored document."""
-        response = requests.post('{}/api/v1/analysis-result'.format(self.host), json=content)
-        response.raise_for_status()
-        return response.json()['document_id']
-
-    def get_result_listing(self):
-        """List all available analysis results stored."""
-        response = requests.get('{}/api/v1/result?type=analysis'.format(self.host))
-        response.raise_for_status()
-        return response.json()['files']
-
-    def is_connected(self) -> bool:
-        return True
-
-    def connect(self):
-        return True
+class AnalysisResultsStore(ResultStorageBase):
+    PATH_ENV_VAR = 'THOTH_CEPH_ANALYSIS_PATH'
