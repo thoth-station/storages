206923a559d7b566219a3ac36e969c170594dc31
Fridolin Pokorny <fridolin@redhat.com>
2018-03-14 13:55:08 +0100

    Version 0.0.7

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_install_requires():
 
 setup(
     name='thoth-storages',
-    version='0.0.6',
+    version='0.0.7',
     description='Storage and database adapters available in project Thoth',
     long_description='Storage and database adapters available in project Thoth',
     author='Fridolin Pokorny',
