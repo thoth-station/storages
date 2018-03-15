5d4add254f7649de43bb0f70cf45adb3716f0423
Fridolin Pokorny <fridolin@redhat.com>
2018-03-15 10:39:28 +0100

    Expose Ceph adapter in package

--- thoth/storages/__init__.py
+++ thoth/storages/__init__.py
@@ -1,5 +1,6 @@
 from .analyses import AnalysisResultsStore
 from .buildlogs import BuildLogsStore
+from .ceph import CephStore
 from .graph import GraphDatabase
 from .result_schema import RESULT_SCHEMA
 from .solvers import SolverResultsStore
