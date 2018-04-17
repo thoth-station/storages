cf8863f30dbd255528bbde75ac10ee16cf73f52c
Fridolin Pokorny <fridolin@redhat.com>
2018-04-17 07:22:50 +0200

    Version 0.0.17

--- thoth/storages/__init__.py
+++ thoth/storages/__init__.py
@@ -1,7 +1,7 @@
 """Storage and database adapters for Thoth."""
 
 __name__ = 'thoth-storages'
-__version__ = '0.0.16'
+__version__ = '0.0.17'
 
 from .analyses import AnalysisResultsStore
 from .buildlogs import BuildLogsStore
