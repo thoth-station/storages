4a10a86ae1dba1f4e8f9e2196888208c90a9cf17
Christoph Görn <goern@redhat.com>
2018-06-29 13:01:06 +0200

    setting more ENV
    Signed-off-by: Christoph Görn <goern@redhat.com>

--- tools/test-setup.sh
+++ tools/test-setup.sh
@@ -2,9 +2,6 @@
 
 # This script will set up our test environment.
 
-LANG=en_US.utf8 
-LC_ALL=en_US.utf8
-
-scl enable rh-python36 -- pipenv install --three --dev
+scl enable rh-python36 -- LANG=en_US.utf8  LC_ALL=en_US.utf8 pipenv install --three --dev
 
 #end.
