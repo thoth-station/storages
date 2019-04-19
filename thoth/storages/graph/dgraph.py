5215fdf7025333385f96d4139ee90b56040463ca
Francesco Murdaca <francesco.murdaca91@gmail.com>
2019-04-19 11:44:07 +0200

    Added ecoystem filter

--- thoth/storages/graph/dgraph.py
+++ thoth/storages/graph/dgraph.py
@@ -697,7 +697,7 @@ class GraphDatabase(StorageBase):
         """Retrieve listing of Python packages known to graph database instance for the given index."""
         query = """
             {
-                f(func: has(%s)) @filter(eq(index_url, "%s")) {
+                f(func: has(%s)) @filter(eq(index_url, "%s") AND eq(ecosystem, python)) {
                     package_name
                 }
             } 
