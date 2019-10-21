5219f77f190ba1183ef050a56e391f1768c9ce0e
Bissenbay <bissenbay@gmail.com>
2019-10-21 16:46:05 -0400

    Get internal software & hardware environments

--- thoth/storages/graph/postgres.py
+++ thoth/storages/graph/postgres.py
@@ -2970,6 +2970,28 @@ class GraphDatabase(SQLBase):
 
         return [{"url": item[0], "warehouse_api_url": item[1], "verify_ssl": item[2]} for item in query.all()]
 
+    def get_hardware_environments(self, is_external: bool = False) -> List[Dict]:
+        """Get hardware environments (external or internal) registered in the graph database."""
+        if is_external:
+            hardware_environment = ExternalHardwareInformation
+        else:
+            hardware_environment = HardwareInformation
+
+        result = self._session.query(hardware_environment).all()
+
+        return [model.dict() for model in result]
+
+    def get_software_environments(self, is_external: bool = False) -> List[Dict]:
+        """Get software environments (external or internal) registered in the graph database."""
+        if is_external:
+            software_environment = ExternalSoftwareEnvironment
+        else:
+            software_environment = SoftwareEnvironment
+
+        result = self._session.query(software_environment).all()
+
+        return [model.dict() for model in result]
+
     def get_python_package_index_urls(self, enabled: bool = None) -> set:
         """Retrieve all the URLs of registered Python package indexes."""
         query = self._session.query(PythonPackageIndex)
