dd2a60bc22a334295b1900244fcc297cd0c9ecb8
Francesco Murdaca <fmurdaca@redhat.com>
2021-06-11 16:31:33 +0200

    Sync solver name, no mapping required
    Signed-off-by: Francesco Murdaca <fmurdaca@redhat.com>

--- thoth/storages/graph/postgres.py
+++ thoth/storages/graph/postgres.py
@@ -5091,7 +5091,7 @@ class GraphDatabase(SQLBase):
                 ecosystem="python",
                 solver_name=solver_name,
                 solver_version=solver_version,
-                os_name=map_os_name(os_name),
+                os_name=os_name,
                 os_version=normalize_os_version(os_name, os_version),
                 python_version=python_version,
             )
