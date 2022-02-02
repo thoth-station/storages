054a7bac0c0aa1923c11d1366467aefe60b7a4b7
mayaCostantini <mcostant@redhat.com>
2022-02-02 23:02:31 +0100

    Revert to filters

--- thoth/storages/graph/postgres.py
+++ thoth/storages/graph/postgres.py
@@ -3039,9 +3039,8 @@ class GraphDatabase(SQLBase):
 
         if package_name:
             query = (
-                query.join(PackageExtractRun)
-                .join(Identified)
-                .join(PythonPackageVersionEntity)
+                query.filter(PackageExtractRun.id == Identified.package_extract_run_id)
+                .filter(Identified.python_package_version_entity_id == PythonPackageVersionEntity.id)
                 .filter(PythonPackageVersionEntity.package_name == package_name)
             )
 
