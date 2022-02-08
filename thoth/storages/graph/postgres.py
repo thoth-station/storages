a0a789c6fe547cb409b7eae3367f784aea1c4254
mayaCostantini <mcostant@redhat.com>
2022-02-08 20:05:19 +0100

    Query datetime of last container image synced in the database

--- thoth/storages/graph/postgres.py
+++ thoth/storages/graph/postgres.py
@@ -426,6 +426,12 @@ class GraphDatabase(SQLBase):
 
         return max([datetime[0] for datetime in result.all()])
 
+    def get_last_image_datetime(self) -> datetime:
+        """Get the datetime of the last container image analysis synced in the database."""
+        with self._session_scope() as session:
+            result = session.query(func.max(PackageExtractRun.datetime))
+            return result.first()[0]
+
     @staticmethod
     def normalize_python_package_name(package_name: str) -> str:
         """Normalize Python package name based on PEP-0503."""
